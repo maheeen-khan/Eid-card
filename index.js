578784dc73ed5d35efa0aa6c8c1e02812c1cdbbd
maheeen-khan <maheeenkhan4@gmail.com>
2024-06-21 16:50:47 +0500

    completed

--- index.js
+++ index.js
@@ -5,19 +5,19 @@ var userNameCapitalize = "";
 
 
 // Capitalize first letter of Full name
-for(var i = 0; i < userName.length; i++){
-    if(i == 0){
+for (var i = 0; i < userName.length; i++) {
+    if (i == 0) {
         userNameCapitalize += userName[i].toUpperCase();
     }
-    else if (userName[i] === " "){
+    else if (userName[i] === " ") {
         userNameCapitalize += " ";
         i++;
         userNameCapitalize += userName[i].toUpperCase();
     }
-    else{
+    else {
         userNameCapitalize += userName[i];
     }
 }
 
 
-document.write("<div class='card text-center mx-auto mt-5 animate__animated animate__bounceInUp' style='width: 18rem;'>" + "<img src='image/eid.jpg' class='card-img-top' alt='Eid image'>" + "<div class='card-body'>" + "<h2 class='head'>" + "EID MUBARAK" + "</h2>" + "<p class='card-text'>" + "<p class = 'fst-italic userN'>" + userNameCapitalize + "</p>" + " from " + "<span class='name'>" + myName + "</span>"/* + "and her family."*/ + "<hr> <span class='bottom'>" + "May the guidance and blessings of Allah be with you and your family. " + "<span class='wish'>" + "Eid Mubarak!" + "</span></span> 🕌❤️" + "</p>" + "</div>" +  "</div>");
+document.write("<div class='card text-center mx-auto mt-5 animate__animated animate__bounceInUp main-card' style='width: 18rem;'>" + "<img src='image/eid.jpg' class='card-img-top' alt='Eid image'>" + "<div class='card-body'>" + "<h2 class='head'>" + "Eid Mubarak" + "</h2>" + "<p class='card-text'>" + "<p class = 'fst-italic userN'>" + userNameCapitalize + "</p>" + " from " + "<span class='name'>" + myName + "</span>"/* + "and her family."*/ + "<hr> <span class='bottom'>" + "May the guidance and blessings of Allah be with you and your family. " + "<span class='wish'>" + "Eid Mubarak!" + "</span></span> 🕌❤️" + "</p>" + "</div>" + "</div>");
