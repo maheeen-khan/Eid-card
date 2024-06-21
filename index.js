970e3689e8a91acbc8a8ecd73f52ac875cf26421
maheeen-khan <maheeenkhan4@gmail.com>
2024-06-21 16:21:28 +0500

    update

--- index.js
+++ index.js
@@ -1,19 +1,23 @@
-var n = prompt("Enter your name : ");
+var myName = "maheen khan";
 
-// n = n.toUpperCase();
+var userName = prompt("Enter your name : ");
 var userNameCapitalize = "";
 
-for(var i = 0; i < n.length; i++){
+
+// Capitalize first letter of Full name
+for(var i = 0; i < userName.length; i++){
     if(i == 0){
-        userNameCapitalize += n[i].toUpperCase();
+        userNameCapitalize += userName[i].toUpperCase();
     }
-    else if (n[i-1] === " "){
-       
-        userNameCapitalize += n[i].toUpperCase();
+    else if (userName[i] === " "){
+        userNameCapitalize += " ";
+        i++;
+        userNameCapitalize += userName[i].toUpperCase();
     }
     else{
-        userNameCapitalize += n[i];
+        userNameCapitalize += userName[i];
     }
 }
 
-document.write("<div class='card text-center mx-auto mt-5 animate__animated animate__bounceInUp' style='width: 18rem;'>" + "<img src='image/eid.jpg' class='card-img-top' alt='Eid image'>" + "<div class='card-body'>" + "<h2 class='head'>" + "EID MUBARAK" + "</h2>" + "<p class='card-text'>" + "<p class = 'fst-italic'>" + userNameCapitalize + "</p>" + " from " + "<span class='name'>" + "maheen khan " + "</span>" + "and her family." + "<hr> <span class='bottom'>" + "May the guidance and blessings of Allah be with you and your family. " + "<span class='wish'>" + "Eid Mubarak!" + "</span></span> 🕌❤️" + "</p>" + "</div>" +  "</div>");
+
+document.write("<div class='card text-center mx-auto mt-5 animate__animated animate__bounceInUp' style='width: 18rem;'>" + "<img src='image/eid.jpg' class='card-img-top' alt='Eid image'>" + "<div class='card-body'>" + "<h2 class='head'>" + "EID MUBARAK" + "</h2>" + "<p class='card-text'>" + "<p class = 'fst-italic userN'>" + userNameCapitalize + "</p>" + " from " + "<span class='name'>" + myName + "</span>"/* + "and her family."*/ + "<hr> <span class='bottom'>" + "May the guidance and blessings of Allah be with you and your family. " + "<span class='wish'>" + "Eid Mubarak!" + "</span></span> 🕌❤️" + "</p>" + "</div>" +  "</div>");
